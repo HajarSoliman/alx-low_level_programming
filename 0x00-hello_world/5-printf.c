06b8d0b2202f41e85e997c14728ffabc4e070736
HajarSoliman <hajar-muhammad@hotmail.com>
2023-10-06 15:35:23 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /* main - Prints "with proper grammar, but the outcome is a piece of art," */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
